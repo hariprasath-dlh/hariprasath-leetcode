f2a2e038da35da7fc38653a9b5a2ef8c1ab6adbf
Hariprasath L V <hariprasathdlhdlh@gmail.com>
2026-07-14 15:14:17 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,9 @@
+// Last updated: 14/07/2026, 15:14:15
+class Solution {
+    public int accountBalanceAfterPurchase(int p) {
+     int n=((p + 5) / 10) * 10;
+     int ans = 100-n;
+     return ans;
+    }
+    
+}
